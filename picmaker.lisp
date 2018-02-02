ac51415569f0940ca9c2b3d4e02955a381fb2e2f
kchen29 <kchen29@stuy.edu>
2018-02-02 17:45:05 -0500

    cleaned up code

--- picmaker.lisp
+++ picmaker.lisp
@@ -1,23 +1,20 @@
 ;;assume P3
 ;;size - (width height)
-;;pixels - arrays of arrays of pixels
-;;actually just a list of pixels for now
+;;pixels - a list of pixels for now
 ;;each pixel is a list '(R G B)
-(defparameter *lst* '())
-
 (defun write-ppm (filename size pixels)
   (with-open-file (stream filename :direction :output :if-exists :supersede)
     (format stream
             "P3 ~a ~a 255 ~{~%~{~a ~a ~a ~}~}"
             (first size) (second size) pixels)))
 
-;;populate lst
-(dotimes (b 4)
-  (dotimes (g 256)
-    (dotimes (r 256)
-      (setf *lst* (cons (list r g (* b 64)) *lst*)))))
-(setf *lst* (reverse *lst*))
-;;(print *lst*)
+;;generate the pixels
+(defun make-pixels ()
+  (let ((pixels '()))
+    (dotimes (b 4)
+      (dotimes (g 256)
+        (dotimes (r 256)
+          (push (list r g (* b 64)) pixels))))
+    (reverse pixels)))
 
-(write-ppm "output.ppm" '(512 512)
-           *lst*)
+(write-ppm "output.ppm" '(512 512) (make-pixels))
